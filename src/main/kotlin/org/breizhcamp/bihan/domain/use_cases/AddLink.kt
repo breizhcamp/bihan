852e7d5d79c84d06471309602cb2765ce9e9e942
Alex Thomazo <alex@thomazo.info>
2025-09-27 22:32:00 +0200

    Add some logging when creating link

--- src/main/kotlin/org/breizhcamp/bihan/domain/use_cases/AddLink.kt
+++ src/main/kotlin/org/breizhcamp/bihan/domain/use_cases/AddLink.kt
@@ -1,14 +1,21 @@
 package org.breizhcamp.bihan.domain.use_cases
 
+import mu.KotlinLogging
 import org.breizhcamp.bihan.domain.use_cases.ports.LinkPort
 import org.springframework.stereotype.Service
 import java.time.Instant
+
+private val logger = KotlinLogging.logger {}
 
 @Service
 class AddLink(
     private val linkPort: LinkPort,
 ) {
 
-    fun add(url: String, expirationDate: Instant?, id: String?): String = linkPort.addLink(url, expirationDate, id).id
+    fun add(url: String, expirationDate: Instant?, id: String?): String {
+        val createdId = linkPort.addLink(url, expirationDate, id).id
+        logger.info { "Adding link [${createdId}] to $url" }
+        return createdId
+    }
 
 }
